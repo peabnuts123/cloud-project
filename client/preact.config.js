4b2d7be17df517a447d2de2d0e658837a665082a
peabnuts123 <peabnuts123@gmail.com>
2019-09-10 12:05:23 +1200

    [Client] Slightly reformat preact config

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,17 +1,28 @@
-import { resolve } from "path";
+import path from "path";
 
 export default function (config, env, helpers) {
   // Alias paths
   config.resolve = config.resolve || {};
   config.resolve.alias = config.resolve.alias || {};
-  config.resolve.alias['@app'] = resolve(__dirname, 'src/');
 
-  // Use any `index` file, not just index.js
-  config.resolve.alias["preact-cli-entrypoint"] = resolve(
-    process.cwd(),
-    "src",
-    "index"
-  );
+  Object.assign(config.resolve.alias, {
+    // `@app` resolves the project root
+    "@app": path.resolve(__dirname, 'src/'),
+    // Use any `index` file, not just index.js
+    "preact-cli-entrypoint": path.resolve(
+      process.cwd(),
+      "src",
+      "index",
+    ),
+  });
+
+  // @TODO not working
+  // Set custom HTML template
+  // helpers.setHtmlTemplate(config, 'src/index.html');
+
+  // let { plugin: htmlWebpackPluginInstance } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0]
+  // htmlWebpackPluginInstance.options.template = `!!ejs-loader!${path.join(__dirname, 'src/index.html')}`;
+  // console.log(`htmlWebpackPluginInstance`, htmlWebpackPluginInstance);
 
   return config;
 }
