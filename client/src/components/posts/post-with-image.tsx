226c47a6302142acf5eab205ef2f5794c78a5eac
peabnuts123 <peabnuts123@gmail.com>
2019-09-14 22:53:19 +1200

    [Client] Correct indentation in PostWithImage component

--- client/src/components/posts/post-with-image.tsx
+++ client/src/components/posts/post-with-image.tsx
@@ -12,29 +12,29 @@ export default class PostWithImage extends Component<Props, State> {
     return (
       <div class="Post PostWithImage">
         {/* Post image */}
-        <img class="PostWithImage-image" src="https://images.unsplash.com/photo-1462993340984-49bd9e0f32dd" alt="Image attached to post"/>
+        <img class="PostWithImage-image" src="https://images.unsplash.com/photo-1462993340984-49bd9e0f32dd" alt="Image attached to post" />
 
         {/* Post body */}
         <div class="Post-bodyContainer">
-            {/* Avatar column */}
-            <div class="Post-avatarColumn">
-              <div class="Avatar Avatar--large" style="background-image: url(https://images.unsplash.com/photo-1515921273796-42aa45543058)" />
+          {/* Avatar column */}
+          <div class="Post-avatarColumn">
+            <div class="Avatar Avatar--large" style="background-image: url(https://images.unsplash.com/photo-1515921273796-42aa45543058)" />
+          </div>
+
+          {/* Content column */}
+          <div class="Post-contentColumn">
+            {/* Title bar */}
+            <div class="Post-titleBar">
+              <strong class="Post-handle">i-need-to-get-outside-more</strong>
+              <span class="Post-timestamp">24m ago</span>
             </div>
 
-            {/* Content column */}
-            <div class="Post-contentColumn">
-              {/* Title bar */}
-              <div class="Post-titleBar">
-                <strong class="Post-handle">i-need-to-get-outside-more</strong>
-                <span class="Post-timestamp">24m ago</span>
-              </div>
+            {/* Message */}
+            <p class="Post-message">Here is a nice photo I found of what outside looks like apparently. Maybe it is worth considering?</p>
 
-              {/* Message */}
-              <p class="Post-message">Here is a nice photo I found of what outside looks like apparently. Maybe it is worth considering?</p>
-
-              {/* Social bar */}
-              <SocialBar numComments={1318} />
-            </div>
+            {/* Social bar */}
+            <SocialBar numComments={1318} />
+          </div>
         </div>
       </div>
     );
