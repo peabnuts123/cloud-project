23a1794d9f20a7f2da50cedbd903f07881558206
peabnuts123 <peabnuts123@gmail.com>
2019-09-14 18:22:07 +1200

    Move generic post styles into post style component

--- client/src/components/posts/post-with-image.tsx
+++ client/src/components/posts/post-with-image.tsx
@@ -10,27 +10,27 @@ export default class PostWithImage extends Component<Props, State> {
 
   public render() {
     return (
-      <div class="PostWithImage">
+      <div class="Post PostWithImage">
         {/* Post image */}
         <img class="PostWithImage-image" src="https://images.unsplash.com/photo-1462993340984-49bd9e0f32dd" alt="Image attached to post"/>
 
         {/* Post body */}
-        <div class="PostWithImage-bodyContainer">
+        <div class="Post-bodyContainer">
             {/* Avatar column */}
-            <div class="PostWithImage-avatarColumn">
+            <div class="Post-avatarColumn">
               <div class="Avatar Avatar--large" style="background-image: url(https://images.unsplash.com/photo-1515921273796-42aa45543058)" />
             </div>
 
             {/* Content column */}
-            <div class="PostWithImage-contentColumn">
+            <div class="Post-contentColumn">
               {/* Title bar */}
-              <div class="PostWithImage-titleBar">
-                <strong class="PostWithImage-handle">i-need-to-get-outside-more</strong>
-                <span class="PostWithImage-timestamp">24m ago</span>
+              <div class="Post-titleBar">
+                <strong class="Post-handle">i-need-to-get-outside-more</strong>
+                <span class="Post-timestamp">24m ago</span>
               </div>
 
               {/* Message */}
-              <p class="PostWithImage-message">Here is a nice photo I found of what outside looks like apparently. Maybe it is worth considering?</p>
+              <p class="Post-message">Here is a nice photo I found of what outside looks like apparently. Maybe it is worth considering?</p>
 
               {/* Social bar */}
               <SocialBar numComments={1318} />
